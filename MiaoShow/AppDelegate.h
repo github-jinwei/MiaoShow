65579f12b4bec5d5c11f8638a65e6f5d9653ffd0
wei jin <jinwei@jinweidemacbook-pro.local>
2016-07-23 21:06:49 +0800

    Initial Commit

---
+++ MiaoShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiaoShow
+//
+//  Created by jinwei on 16/7/23.
+//  Copyright © 2016年 jinwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiaoShow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MiaoShow
+//
+//  Created by jinwei on 16/7/23.
+//  Copyright © 2016年 jinwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
